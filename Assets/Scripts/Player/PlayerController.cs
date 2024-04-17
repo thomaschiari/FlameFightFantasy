7a08ad60a3b36ac2f4471778d1614a2f8c98bf76
FeMCDias <femcdias@gmail.com>
2024-04-17 06:50:15 -0300

    Adding Tilemaps for Collision and Normal + Layer Order

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,56 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float moveSpeed;
-
     private int health = 3;
 
-    private bool isMoving;
+    private void Update()
+    {
+        // Get the input from the horizontal and vertical axes
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
-    private Vector2 input;
+        // Create a movement vector and normalize it
+        Vector2 movement = new Vector2(horizontal, vertical).normalized;
 
-    private void OnTriggerEnter(Collider other)
+        // Apply the movement to the player's position
+        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
             health--;
-            // print debug message
             Debug.Log("Player health: " + health);
         }
     }
-
-    private void Update()
-    {
-        if (!isMoving)
-        {
-            input.x = Input.GetAxisRaw("Horizontal");
-            input.y = Input.GetAxisRaw("Vertical");
-
-            if (input.x != 0) input.y = 0;
-            
-
-            if (input != Vector2.zero)
-            {
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-
-                StartCoroutine(Move(targetPos));
-            }
-        }
-    }
-
-    IEnumerator Move(Vector3 targetPos)
-    {
-        isMoving = true;
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPos;
-        isMoving = false;
-    }
-
 }
