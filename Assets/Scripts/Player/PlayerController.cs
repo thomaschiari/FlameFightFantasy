51357f8f216b233393b9b012d78ceafafec097c5
FeMCDias <femcdias@gmail.com>
2024-04-17 07:33:49 -0300

    Perfect movement implemented
    Based on https://www.youtube.com/watch?v=7iYWpzL9GkM&ab_channel=Chris%27Tutorials

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,23 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 3f;
+    public float collisionOffset = 0.1f;
+    public ContactFilter2D movementFilter;
+
+    Vector2 moveInput;
     private int health = 3;
+    private Rigidbody2D rb;
 
-    private void Update()
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>(); // List to store all the collisions
+
+
+    private void Start()
     {
-        // Get the input from the horizontal and vertical axes
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-        // Create a movement vector and normalize it
-        Vector2 movement = new Vector2(horizontal, vertical).normalized;
+    private void FixedUpdate()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            bool success = TryMove(moveInput); // Try to move in any direction of input (could be diagonal)
+            if (!success)
+            {
+                TryMove(new Vector2(moveInput.x, 0)); // Try to move horizontally
+                if (!success)
+                {
+                    TryMove(new Vector2(0, moveInput.y)); // Try to move vertically
+                }
+            }
 
-        // Apply the movement to the player's position
-        transform.Translate(movement * moveSpeed * Time.deltaTime, Space.World);
+        }
+    }
+
+    private bool TryMove(Vector2 direction){
+        int count = rb.Cast( // Cast a box in the direction we want to move
+            direction,
+            movementFilter,
+            castCollisions,
+            moveSpeed * Time.fixedDeltaTime + collisionOffset
+        );
+
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        return false;
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        moveInput = movementValue.Get<Vector2>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -28,4 +67,6 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Player health: " + health);
         }
     }
+
+    
 }
